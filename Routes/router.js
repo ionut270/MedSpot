be7d52dcbcc62182a16f0554bff66522db19537e
Marius Dinu <dinumariusciprian@gmail.com>
2021-05-31 02:10:33 +0300

    Search

--- Routes/router.js
+++ Routes/router.js
@@ -1,10 +1,4 @@
 module.exports = (app)=>{
-<<<<<<< Updated upstream
-    // handler for /profile route
     require('./user.js')(app);
-=======
-    app.post('/upload/id_card',(req,res)=>{ upload(req,res); });
-    app.get('/profile',(req,res)=>{ require('./user.js')(req,res) });
     app.post('/search',(req,res)=>{require('./search.js')(req,res)});
->>>>>>> Stashed changes
 }
