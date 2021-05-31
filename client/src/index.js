5353259f6648d6ac16abb56f00918f77c84b8018
ionut270 <oanceaionut15@gmail.com>
2021-06-01 00:42:03 +0300

    Cleaned code

--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,8 @@
-import React                                      from 'react';
-import ReactDOM                                   from 'react-dom';
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 // Import used components
-import App  from './App/app_manager';
+import App from './App/app_manager';
 import Auth from './Auth/auth';
 import Loading from './Loading/loading';
 import Complete from './Complete/complete'
@@ -20,19 +19,20 @@ class Comp extends React.Component {
     this.state = { auth: null, complete: null, user: null, loading: false }
     this.loading = this.loading.bind(this);
   }
-  async componentDidMount(){
-    var session = await utils.request('/session','GET');
-    this.setState({auth: session.auth, complete: session.complete, user : session.user})
+  async componentDidMount() {
+    var session = await utils.request('/session', 'GET');
+    this.setState({ auth: session.auth, complete: session.complete, user: session.user })
   }
 
-  loading(state){ this.setState({loading: state==='on' ? true : false}) }
+  loading(state) { this.setState({ loading: state === 'on' ? true : false }) }
 
-  render() {    
-    if(this.state.auth === null || this.state.loading ) return <Loading />
-    if(this.state.auth === true && this.state.complete === false) return <Complete user={this.state.user} loading={this.loading}  />
-    if (this.state.auth === true) return <App />
-    return <Auth />
+  render() {
+    const { auth, loading, complete, user } = this.state;
+    if (auth === null || loading)             return <Loading />
+    if (auth === true && complete === false)  return <Complete user={user} loading={this.loading} />
+    if (auth === true)                        return <App />
+                                              return <Auth />
   }
 }
 
-ReactDOM.render( <React.StrictMode><Comp /></React.StrictMode>,document.getElementById('root') );
+ReactDOM.render(<Comp />, document.getElementById('root'));
